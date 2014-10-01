81056ca31f54b3b2bbe89cdf8863182e32c3397c
YohanPeslier <yohanpeslier>
2014-10-01 14:37:05 +0200

    test
    test 1

---
+++ mod_MUBAOZ_Anonyme_Download/helper.php
@@ -0,0 +1,23 @@
+<?php
+
+class modHelloWorldHelper
+
+{
+
+/**
+* Retrieves the hello message
+* @param array $params An object containing the module parameters
+* @access public
+*/
+
+public static function getHello( $params )
+
+{
+
+return 'Hello, World!';
+
+}
+
+}
+
+?>
---
+++ mod_MUBAOZ_Anonyme_Download/mod_MUBAOZ_Anonyme_Download.php
@@ -0,0 +1,21 @@
+<?php
+
+/*******************************
+* Hello World! Module Entry Point
+*******************************/
+
+// no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Include the syndicate functions only once
+
+require_once( dirname(__FILE__).'/helper.php' );
+
+ 
+
+$hello = modHelloWorldHelper::getHello( $params );
+
+require( JModuleHelper::getLayoutPath( 'mod_helloworld' ) );
+
+?>
